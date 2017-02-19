ee7d1312a23f3e6c75dad0ba510ad932ac7ce87d
Garren Rose <garrenrose@10.0.1.38>
2017-02-18 21:01:02 -0700

    2.0 working version of open-source Cryptomight app.
    Contains three methods of encryption with customizable key variables.
    1) Caesar Cipher Encryption
    2) Affine Cipher Encryption 
    3) Great Cipher + Affine Cipher Encryption (My own cocktail)

---
+++ src/Cryptomight_v2_proj/Cryptomight_v2.java
@@ -0,0 +1,23 @@
+package Cryptomight_v2_proj;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author garrenrose
+ */
+public class Cryptomight_v2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CaesarFrame form = new CaesarFrame();
+            form.setVisible(true);
+        // TODO code application logic here
+    }    
+}
